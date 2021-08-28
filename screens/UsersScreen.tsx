bf121e7212f126530172da534e66d5a24733ce0b
Flávio Ferreira <business.flavioferreira@gmail.com>
2021-08-28 14:58:28 -0300

    Creating new chat and connecting the users

--- screens/UsersScreen.tsx
+++ screens/UsersScreen.tsx
@@ -1,15 +1,32 @@
-import * as React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Text, Image, View, StyleSheet, FlatList, Pressable } from 'react-native';
 
 import Users from '../assets/dummy-data/Users';
+import { DataStore } from '@aws-amplify/datastore';
+import { User } from '../src/models';
+
 import UserItem from '../components/UserItem';
 
 export default function UsersScreen() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    DataStore.query(User).then(setUsers)
+  }, []);
+
+  // useEffect(() =>{
+  //   const fetchUsers = async () => {
+  //     const fetchedUsers = await DataStore.query(User);
+  //     setUsers(fetchedUsers);
+  //   }
+
+  //   fetchUsers()
+  // }, [])
 
   return (
     <View style={styles.page}>
       <FlatList 
-        data={Users}
+        data={users}
         showsVerticalScrollIndicator={false}
         renderItem={({ item }) => <UserItem user={item}/>}
       />
